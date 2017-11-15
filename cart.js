af88ac475fc1d2e5810f2ea8b77f122530c6e81f
dobiedad <leomdiv@gmail.com>
2017-11-15 12:06:30 +0000

    working cart

---
+++ cart.js
@@ -0,0 +1,62 @@
+class Cart {
+  constructor(props) {
+    !props ? props = {} : undefined
+    this.items = props.items || {}
+  }
+
+  add(product) {
+    const itemExistsInCart = this.items[product.id]
+
+    if(itemExistsInCart){
+      const itemInCartHasNoQty = !this.items[product.id].qty
+      if(itemInCartHasNoQty) {
+        this.resetQuantity(product)
+      }
+      return Promise.resolve(this.items[product.id].qty++)
+    }
+    this.items[product.id] = product
+    return Promise.resolve(this.resetQuantity(product))
+  }
+
+  remove(product) {
+    const item = this.items[product.id]
+
+    if(item && item.qty == 1){
+      delete this.items[product.id]
+    }
+    else if (item) {
+      item.qty--
+    }
+    return Promise.resolve()
+  }
+
+  total() {
+    var total = 0
+
+    Object.keys(this.items).map((key) => {
+      total += (Number(this.items[key].price) * Number(this.items[key].qty))
+    });
+
+    return total
+  }
+
+  count() {
+    var count = 0
+
+    Object.keys(this.items).map((key) => {
+      count += this.items[key].qty
+    });
+
+    return count
+  }
+
+  resetQuantity(product) {
+    this.items[product.id].qty = 1
+  }
+
+  reset() {
+    this.items = {}
+  }
+}
+
+export { Cart as default }
---
+++ cartSpec.js
@@ -0,0 +1,69 @@
+import Cart from './cart'
+import { expect } from 'chai'
+
+var cart
+
+const products = {
+  apple:{name:'Apple',price:'1.99',id:'a'},
+  pear:{name:'Pear',price:'2.50',id:'b'},
+  carrot:{name:'Carrot',price:'0.99',id:'c'}
+}
+
+describe('Shopping Cart',() => {
+
+  beforeEach(() => {
+    cart = new Cart()
+  })
+
+  it('Adds a product to the cart',() => {
+    return cart.add(products.apple)
+      .then(() => {
+        expect(cart.items[products.apple.id]).to.eql(products.apple)
+      })
+  })
+
+  it('Adds multiple quantities of the same product',() => {
+    return Promise.all([cart.add(products.apple),cart.add(products.apple)])
+      .then(() => {
+        expect(cart.items[products.apple.id].qty).to.eql(2)
+      })
+  })
+
+  it('Adds different products',() => {
+    return Promise.all([cart.add(products.apple),cart.add(products.pear)])
+      .then(() => {
+        expect(cart.items[products.apple.id].qty).to.eql(1)
+        expect(cart.items[products.pear.id].qty).to.eql(1)
+      })
+  })
+
+  it('Calculates total items in the cart',() => {
+    return Promise.all([cart.add(products.apple),cart.add(products.pear),cart.add(products.pear)])
+      .then(() => {
+        expect(cart.count()).to.eql(3)
+      })
+  })
+
+  it('Calculates total cost for the items in the cart',() => {
+    return Promise.all([cart.add(products.apple),cart.add(products.pear),cart.add(products.pear)])
+      .then(() => {
+        expect(cart.total()).to.eql(6.99)
+      })
+  })
+
+  it('Removes a product',() => {
+    return Promise.all([cart.add(products.apple),cart.add(products.pear),cart.remove(products.apple)])
+      .then(() => {
+        expect(cart.count()).to.eql(1)
+      })
+  })
+
+  it('Resets',() => {
+    return Promise.all([cart.add(products.apple)])
+      .then(() => {
+        cart.reset()
+        expect(cart.count()).to.eql(0)
+      })
+  })
+
+})
